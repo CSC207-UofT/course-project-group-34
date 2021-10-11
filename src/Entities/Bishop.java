85b9d3aeb527ba0fb281246a2e085f906969a029
akazee <56356510+akazee@users.noreply.github.com>
2021-10-11 16:06:40 -0400

    Update Bishop.java

--- src/Entities/Bishop.java
+++ src/Entities/Bishop.java
@@ -1,1 +1,7 @@
-public class Bishop extends ChessPiece {}
+public class Bishop extends ChessPiece {
+  
+  public Bishop(int xPosition, int yPosition, String color){
+    super(xPosition, yPosition, color );
+  };
+  
+}
