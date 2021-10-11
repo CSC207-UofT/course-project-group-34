3c437f97b51140dd5c6a2d6797b3fb70f0eca4b5
akazee <56356510+akazee@users.noreply.github.com>
2021-10-11 17:02:34 -0400

    Create CLIBoard.java

---
+++ src/UI/CLIBoard.java
@@ -0,0 +1,21 @@
+/**
+* ik its not done just ignore this for now im doing it on intelliJ
+*/
+public class CLIBoard {
+  
+  //private GameState gameState;
+  
+  public CLIBoard(GameState gameState){
+    //this.gameState = gameState
+  }
+  
+  
+  public class printBoard(Gamestate gameState){
+   
+    // prints the board based on the given gamestate
+    
+    
+    
+  }
+
+}
