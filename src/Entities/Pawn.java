d5d9d11c30d52ca2683247e20db61d8798ce6118
akazee <56356510+akazee@users.noreply.github.com>
2021-10-11 16:42:26 -0400

    Update Pawn.java

--- src/Entities/Pawn.java
+++ src/Entities/Pawn.java
@@ -1,7 +1,18 @@
 public class Pawn extends ChessPiece { 
+  
+  private bool hasMovedOnce;
 
   public Pawn(int xPosition, int yPosition, String color){
     super(xPosition, yPosition, color );
+    this.hasMovedOnce = false;
   };
+  
+  public bool getHasMovedOnce(){
+    return this.hasMovedOnce;
+  }
+  
+  public void setHasMovedOnce(bool cond){
+    this.hasMovedOnce = cond;
+  }
   
 }
