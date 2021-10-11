50e2237c105e3da348fc54087715a2e9722275d7
akazee <56356510+akazee@users.noreply.github.com>
2021-10-11 16:08:22 -0400

    Update Queen.java

--- src/Entities/Queen.java
+++ src/Entities/Queen.java
@@ -1,1 +1,8 @@
-public class Queen extends ChessPiece {}
+public class Queen extends ChessPiece {
+  
+  public Queen(int xPosition, int yPosition, String color){
+      super(xPosition, yPosition, color );
+    };
+  
+
+}
