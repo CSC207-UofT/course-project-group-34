307c55021c3f5306e8cf714d0e62386b456e0a7f
Parth Amin <aminparth369@gmail.com>
2021-10-11 16:50:08 -0400

    initial pawn move

--- src/Entities/Pawn.java
+++ src/Entities/Pawn.java
@@ -1,18 +1,28 @@
 public class Pawn extends ChessPiece { 
   
-  private bool hasMovedOnce;
+  private boolean hasMovedOnce;
+  private boolean madeMove;
 
   public Pawn(int xPosition, int yPosition, String color){
     super(xPosition, yPosition, color );
     this.hasMovedOnce = false;
-  };
+    this.madeMove = false;
+  }
   
-  public bool getHasMovedOnce(){
+  public boolean getHasMovedOnce(){
     return this.hasMovedOnce;
   }
   
-  public void setHasMovedOnce(bool cond){
+  public void setHasMovedOnce(boolean cond){
     this.hasMovedOnce = cond;
+  }
+
+  public boolean getMadeMove() {
+    return madeMove;
+  }
+
+  public void setMadeMoveTrue() {
+    madeMove = true;
   }
   
 }
