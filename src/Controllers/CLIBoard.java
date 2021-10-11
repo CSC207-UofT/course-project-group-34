61849b798ae11b5429e22f76385b208148bb30c3
akazee <56356510+akazee@users.noreply.github.com>
2021-10-11 16:28:28 -0400

    Update CLIBoard.java

--- src/Controllers/CLIBoard.java
+++ src/Controllers/CLIBoard.java
@@ -1,7 +1,23 @@
 /**
-*
+* ik its not done just ignore this for now im doing it on intelliJ
 */
 public class CLIBoard {
+  
+  //private GameState gameState;
+  
+  public CLIBoard(GameState gameState){
+    //this.gameState = gameState
+  }
+  
+  
+  public class printBoard(Gamestate gameState){
+   
+    // prints the board based on the given gamestate
+    
+    
+    
+    
+  }
 
 
 
