f0ce29bbf1e3057f0aa07a98c67cf95b6cb8d2ab
akazee <56356510+akazee@users.noreply.github.com>
2021-10-11 16:08:38 -0400

    Update Rook.java

--- src/Entities/Rook.java
+++ src/Entities/Rook.java
@@ -1,1 +1,8 @@
-public class Rook extends ChessPiece {}
+public class Rook extends ChessPiece {
+
+  public Rook(int xPosition, int yPosition, String color){
+    super(xPosition, yPosition, color );
+  };
+  
+  
+}
