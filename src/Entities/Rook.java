c522718d67b6ccd6aa605a170b7e64c6372b6c61
akazee <56356510+akazee@users.noreply.github.com>
2021-10-14 03:18:26 -0400

    Update Rook.java
    updated constructor to set letter instance variable

--- src/Entities/Rook.java
+++ src/Entities/Rook.java
@@ -1,8 +1,13 @@
 public class Rook extends ChessPiece {
 
   public Rook(int xPosition, int yPosition, String color){
+
     super(xPosition, yPosition, color );
-  };
-  
-  
+    if(color.equals("black")){
+      super.setLetter('R');
+    }
+    else{
+      super.setLetter('r');
+    }
+  } 
 }
