1d94cfe7e4fcbaf52578f9773ff8465d5897de21
akazee <56356510+akazee@users.noreply.github.com>
2021-10-14 03:17:30 -0400

    Update Queen.java
    updated constructor to set letter instance

--- src/Entities/Queen.java
+++ src/Entities/Queen.java
@@ -2,7 +2,11 @@ public class Queen extends ChessPiece {
   
   public Queen(int xPosition, int yPosition, String color){
       super(xPosition, yPosition, color );
-    };
-  
-
+      if(color.equals("black")){
+          super.setLetter('Q');
+      }
+      else{
+          super.setLetter('q');
+      }
+    }
 }
