f92e85c2dd41b26c1634cf73f41d7754b89d6d07
akazee <56356510+akazee@users.noreply.github.com>
2021-10-14 03:15:33 -0400

    Update ChessPiece.java
    added letter instance variable to abstract class

--- src/Entities/ChessPiece.java
+++ src/Entities/ChessPiece.java
@@ -1,39 +1,46 @@
-package Entities;
-
 public abstract class ChessPiece {
 
-  private int xPosition;
-  private int yPosition;
-  private String color;
-  
-  public ChessPiece(int xPosition, int yPosition, String color) {
-    this.xPosition = xPosition;
-    this.yPosition = yPosition; 
-    this.color = color;
-  }
-  
-  public int getXPosition() {
-    return this.xPosition;
-  }
-  
-  public void setXPosition(int num) {
-    this.xPosition = num;
-  }
-  
-  public int getYPosition() {
-    return this.yPosition;
-  }
-  
-  public void setYPosition(int num) {
-    this.yPosition = num;
-  }
-  
-  public String getColor() {
-    return color;
-  }
-  
-  public void setColor(String color) {
-    this.color = color;
-  }
+    private int xPosition;
+    private int yPosition;
+    private String color;
+    private char letter;
+
+    public ChessPiece(int xPosition, int yPosition, String color) {
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
+        this.color = color;
+    }
+
+    public int getXPosition() {
+        return this.xPosition;
+    }
+
+    public void setXPosition(int num) {
+        this.xPosition = num;
+    }
+
+    public int getYPosition() {
+        return this.yPosition;
+    }
+
+    public void setYPosition(int num) {
+        this.yPosition = num;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setLetter(char letter){
+        this.letter = letter;
+    }
+
+    public char getLetter(){
+        return this.letter;
+    }
 
 }
