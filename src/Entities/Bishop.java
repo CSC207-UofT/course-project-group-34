42e82384eedd008697ac408713f5c6573baf8ca0
akazee <56356510+akazee@users.noreply.github.com>
2021-10-14 03:19:25 -0400

    Update Bishop.java
    updated constructor to set letter instance variable

--- src/Entities/Bishop.java
+++ src/Entities/Bishop.java
@@ -1,7 +1,13 @@
 public class Bishop extends ChessPiece {
   
   public Bishop(int xPosition, int yPosition, String color){
+
     super(xPosition, yPosition, color );
+    if(color.equals("black")){
+      super.setLetter('B');
+    }
+    else{
+      super.setLetter('b');
+    }
   };
-  
 }
