0d96526d1b4788824918b4287a258f44372e4b29
Parth Amin <aminparth369@gmail.com>
2021-10-14 18:11:10 -0400

    unit test for pawn move

--- test/testValidPawnMove.java
+++ test/testValidPawnMove.java
@@ -1,4 +1,6 @@
-public class PawnMoveTest{
+import org.junit.Before;
+import org.junit.Test;
+import java.util.List;
 
 //Create a board 
 
@@ -6,4 +8,22 @@ public class PawnMoveTest{
 
 //test to check if the postion we moved the pawn into is the correct one
 
+}
+
+public class testValidPawnMove {
+
+    GameState state;
+
+    @Before
+    public void setUp() {
+        LoadGame init = new LoadGame();
+        state = init.loadGame();
+    }
+
+    @Test(timeout = 100)
+    public void testMove() {
+        int[] arr = [3, 6, 3, 4];
+        state.makeMove(arr);
+        assertTrue(state.getBoard()[3][6] == null);
+    }
 }
