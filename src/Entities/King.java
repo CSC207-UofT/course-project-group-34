9ff05d1527eb627eda0b00f148c4a79aac141779
akazee <56356510+akazee@users.noreply.github.com>
2021-10-14 03:16:09 -0400

    Update King.java

--- src/Entities/King.java
+++ src/Entities/King.java
@@ -1,7 +1,18 @@
 public class King extends ChessPiece {
 
   public King(int xPosition, int yPosition, String color){
+
     super(xPosition, yPosition, color );
-  };
-  
+
+    // Using the Characters package in order to use unicode to set
+    // Latin characters for K, in order to avoid confusion with
+    // the letters used for Knight chess pieces.
+    // Learned from: https://docs.oracle.com/javase/tutorial/i18n/text/unicode.html
+    if(color.equals("black")){
+      super.setLetter(Character.toChars(0x0198)[0]);
+    }
+    else{
+      super.setLetter(Character.toChars(0x0199)[0]);
+    }
+  }
 }
