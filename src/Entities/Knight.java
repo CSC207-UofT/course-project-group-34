17006fd8fff54429fe78c132c72128d6f8f000da
akazee <56356510+akazee@users.noreply.github.com>
2021-10-14 03:20:42 -0400

    Update Knight.java
    updated constructor to set up letter instance variable

--- src/Entities/Knight.java
+++ src/Entities/Knight.java
@@ -1,7 +1,14 @@
+
 public class Knight extends ChessPiece {
 
   public Knight(int xPosition, int yPosition, String color){
+
     super(xPosition, yPosition, color );
+    if(color.equals("black")){
+      super.setLetter('K');
+    }
+    else{
+      super.setLetter('k');
+    }
   };
-  
 }
