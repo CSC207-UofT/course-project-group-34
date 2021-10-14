2500f5598571c9179ff207d227ba1929288453c6
akazee <56356510+akazee@users.noreply.github.com>
2021-10-14 03:23:27 -0400

    Added basic methods needed to make CLI work

--- src/GameState.java
+++ src/GameState.java
@@ -18,4 +18,20 @@ public class GameState {
     public int getTurn(){
         return this.turn;
     }
+
+    /**
+     * This method adds the ChessPiece object from our parameter and into our
+     * 2D array that represents the chess board.
+     */
+    public void addChessPiece(ChessPiece chessPiece){
+        board[chessPiece.getYPosition()][chessPiece.getXPosition()] = chessPiece;
+    }
+
+    /**
+     * Obtains the "letter" identifier of the chess piece at the given
+     * x and y coordinate. Used in CLIBoard.
+     */
+    public char getChessPieceLetter(int x, int y){
+        return board[x][y].getLetter();
+    }
 }
