f555444a860a4061334a0cc4b40acaea997cfb97
petarjovasevic <80861437+petarjovasevic@users.noreply.github.com>
2021-10-14 16:43:01 -0400

    Update Pawn.java
    Got rid of the madeMove attribute and its related methods; removed the parameter from setHasMovedOnce.

--- src/Entities/Pawn.java
+++ src/Entities/Pawn.java
@@ -1,12 +1,10 @@
 public class Pawn extends ChessPiece {
   
   private boolean hasMovedOnce;
-  private boolean madeMove;
 
   public Pawn(int xPosition, int yPosition, String color){
     super(xPosition, yPosition, color );
     this.hasMovedOnce = false;
-    this.madeMove = false;
     if(color.equals("black")){
       super.setLetter('P');
     }
@@ -19,15 +17,8 @@ public class Pawn extends ChessPiece {
     return this.hasMovedOnce;
   }
   
-  public void setHasMovedOnce(boolean cond){
-    this.hasMovedOnce = cond;
+  public void setHasMovedOnce(){
+    this.hasMovedOnce = true;
   }
 
-  public boolean getMadeMove() {
-    return madeMove;
-  }
-
-  public void setMadeMoveTrue() {
-    madeMove = true;
-  }
 }
