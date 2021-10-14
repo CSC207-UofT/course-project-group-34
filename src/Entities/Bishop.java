b2d4aa1454f236715c8267f41c23dad342b2527e
akazee <56356510+akazee@users.noreply.github.com>
2021-10-14 16:42:45 -0400

    added javadoc

--- src/Entities/Bishop.java
+++ src/Entities/Bishop.java
@@ -1,5 +1,10 @@
+/**
+* This class represents a Bishop chess piece object in our chess game. 
+*/
 public class Bishop extends ChessPiece {
   
+  // The constructor takes in it's (x, y) co-ordinates and the color 
+  // of the player representing this object.
   public Bishop(int xPosition, int yPosition, String color){
 
     super(xPosition, yPosition, color );
