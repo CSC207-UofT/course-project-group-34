25fa2576ef9b663ad4ea1d5503111db91e642e8a
petarjovasevic <80861437+petarjovasevic@users.noreply.github.com>
2021-10-15 19:17:24 -0400

    Update CheckPlayerMove.java

--- src/UseCases/CheckPlayerMove.java
+++ src/UseCases/CheckPlayerMove.java
@@ -5,7 +5,5 @@ import Other.GameState;
 
 public abstract class CheckPlayerMove {
     
-    public CheckPlayerMove() { }
-    
     abstract boolean checkMove(int newRow, int newColumn, ChessPiece piece, GameState gameState);
 }
