17ae6ac0593818be6680e3626550aea90848addb
petarjovasevic <80861437+petarjovasevic@users.noreply.github.com>
2021-10-15 18:36:40 -0400

    Update CheckPawnMove.java

--- src/UseCases/CheckPawnMove.java
+++ src/UseCases/CheckPawnMove.java
@@ -8,61 +8,61 @@ public class CheckPawnMove extends CheckPlayerMove {
 
     public int[][] validMoves(Pawn pawn, GameState gameState) {
         int[][] result = new int[4][2];
-        int xPosition = pawn.getXPosition();
-        int yPosition = pawn.getYPosition();
+        int row = pawn.getRow();
+        int column = pawn.getColumn();
         ChessPiece[][] board = gameState.getBoard();
 
 
         if (pawn.getColor().equals("white")) {
             // Check non-attack moves
-            if (yPosition - 1 > 0 && board[yPosition - 1][xPosition] == null) {
-                result[0][0] = yPosition;
-                result[0][1] = xPosition - 1;
+            if (row - 1 > -1 && board[row - 1][column] == null) {
+                result[0][0] = row - 1;
+                result[0][1] = column;
             }
 
-            if (!pawn.getHasMovedOnce() && yPosition + 2 < 8 && board[xPosition][yPosition + 1] == null && board[xPosition][yPosition + 2] == null) {
-                result[1][0] = xPosition;
-                result[1][1] = yPosition - 2;
+            if (!pawn.getHasMovedOnce() && row - 2 > -1 && board[row - 1][column] == null && board[row - 2][column] == null) {
+                result[1][0] = row - 2;
+                result[1][1] = column;
             }
 
             // Check attack moves
-            if (xPosition - 1 > -1 && yPosition + 1 < 8 && board[xPosition - 1][yPosition + 1] != null) {
-                result[2][0] = xPosition - 1;
-                result[2][1] = yPosition - 1;
+            if (row - 1 > -1 && column - 1 > -1 && board[row - 1][column - 1] != null) {
+                result[2][0] = row - 1;
+                result[2][1] = column - 1;
             }
 
-            if (xPosition + 1 < 8 && yPosition + 1 < 8 && board[xPosition + 1][yPosition + 1] != null) {
-                result[3][0] = xPosition + 1;
-                result[3][1] = yPosition - 1;
+            if (row - 1 > -1 && column + 1 < 8 && board[row - 1][column + 1] != null) {
+                result[3][0] = row - 1;
+                result[3][1] = column + 1;
             }
         } else {
             // Check non-attack moves
-            if (yPosition + 1 < 8 && board[xPosition][yPosition + 1] == null) {
-                result[0][0] = xPosition;
-                result[0][1] = yPosition + 1;
+            if (row + 1 < 8 && board[row + 1][column] == null) {
+                result[0][0] = row + 1;
+                result[0][1] = column;
             }
 
-            if (!pawn.getHasMovedOnce() && yPosition + 2 < 8 && board[xPosition][yPosition + 1] == null && board[xPosition][yPosition + 2] == null) {
-                result[1][0] = xPosition;
-                result[1][1] = yPosition + 2;
+            if (!pawn.getHasMovedOnce() && row + 2 < 8 && board[row + 1][column] == null && board[row + 2][column] == null) {
+                result[1][0] = row + 2;
+                result[1][1] = column;
             }
 
             // Check attack moves
-            if (xPosition - 1 > -1 && yPosition + 1 < 8 && board[xPosition - 1][yPosition + 1] != null) {
-                result[2][0] = xPosition - 1;
-                result[2][1] = yPosition + 1;
+            if (row + 1 < 8 && column - 1 > -1 && board[row + 1][column - 1] != null) {
+                result[2][0] = row + 1;
+                result[2][1] = column - 1;
             }
 
-            if (xPosition + 1 < 8 && yPosition + 1 < 8 && board[xPosition + 1][yPosition + 1] != null) {
-                result[3][0] = xPosition + 1;
-                result[3][1] = yPosition + 1;
-            }
+            if (row + 1 < 8 && column + 1 < 8 && board[row + 1][column + 1] != null) {
+                result[3][0] = row + 1;
+                result[3][1] = column + 1;
         }
+            
         return result;
     }
 
-    public boolean checkMove(int newXPosition, int newYPosition, ChessPiece pawn, GameState gameState) {
-        int[] desiredMove = {newXPosition, newYPosition};
+    public boolean checkMove(int newRow, int newColumn, ChessPiece pawn, GameState gameState) {
+        int[] desiredMove = {newRow, newColumn};
         int[][] possibleMoves = validMoves((Pawn) pawn, gameState);
         for (int[] move : possibleMoves) {
             if (desiredMove.equals(move)) {
