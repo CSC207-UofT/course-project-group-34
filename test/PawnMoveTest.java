58414121ac9f2173d0f12d4fe22555eeada15dc9
akazee <akazee.135@gmail.com>
2021-10-15 20:20:39 -0400

    added clarification to printed statement

--- test/PawnMoveTest.java
+++ test/PawnMoveTest.java
@@ -1,11 +1,14 @@
 import org.junit.Before;
 import org.junit.Test;
-import java.util.List;
 import static org.junit.Assert.*;
 
 import Other.GameState;
 import Other.LoadGame;
 
+/**
+ * This Class is responsible for testing the movements of our Pawn chess
+ * pieces.
+ */
 public class PawnMoveTest {
 
     GameState state;
