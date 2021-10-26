3870fcc24bc1100a518897a3716d721f5247da24
petarjovasevic <80861437+petarjovasevic@users.noreply.github.com>
2021-10-26 16:14:24 -0400

    Implemented validMoves and checkMove for the CheckQueenMove class; changed spacing in CheckPawnMove.checkMove.

--- src/UseCases/CheckPawnMove.java
+++ src/UseCases/CheckPawnMove.java
@@ -65,10 +65,11 @@ public class CheckPawnMove extends CheckPlayerMove {
 
         return result;
     }
-
+     
     public boolean checkMove(int newRow, int newColumn, ChessPiece pawn, GameState gameState) {
         int[] desiredMove = {newRow, newColumn};
         int[][] possibleMoves = validMoves((Pawn) pawn, gameState);
+        
         for (int[] move : possibleMoves) {
             if (Arrays.equals(desiredMove, move)) {
                 return true;
@@ -76,6 +77,5 @@ public class CheckPawnMove extends CheckPlayerMove {
         }
 
         return false;
-
     }
 }
--- src/UseCases/CheckQueenMove.java
+++ src/UseCases/CheckQueenMove.java
@@ -18,6 +18,114 @@ public class CheckQueenMove extends CheckPlayerMove {
         
         // Moving up
         int currentRow = row - 1; 
+        while (currentRow > -1 && board[currentRow][column] == null) {
+            result.add({currentRow, column}); 
+            currentRow--;
+        }
+
+        if (currentRow > -1) { 
+            result.add({currentRow, column}); 
+        }
+
+        // Moving up & right 
+        int currentRow = row - 1;
+        int currentColumn = column + 1;
+        while (currentRow > -1 && currentColumn < 8 && board[currentRow][currentColumn] == null) { 
+            result.add({currentRow, currentColumn});
+            currentRow--;
+            currentColumn++;
+        } 
+
+        if (currentRow > -1 && currentColumn < 8) { 
+            result.add({currentRow, currentColumn}); 
+        }
+
+        // Moving right
+        int currentColumn = column + 1;
+        while (currentColumn < 8 && board[row][currentColumn] == null) {
+            result.add({row, currentColumn});
+            currentColumn++;
+        }
+
+        if (currentColumn < 8) { 
+            result.add({row, currentColumn});
+        }
+
+        // Moving down & right
+        int currentRow = row + 1;
+        int currentColumn = column + 1;
+        while (currentRow < 8 && currentColumn < 8 && board[currentRow][currentColumn] == null) { 
+            result.add({currentRow, currentColumn});
+            currentRow++;
+            currentColumn++;
+        } 
+
+        if (currentRow < 8 && currentColumn < 8) { 
+            result.add({currentRow, currentColumn}); 
+        }
+
+        // Moving down
+        int currentRow = row + 1; 
+        while (currentRow < 8 && board[currentRow][column] == null) {
+            result.add({currentRow, column}); 
+            currentRow++;
+        }
+
+        if (currentRow < 8) { 
+            result.add({currentRow, column}); 
+        }
+
+        // Moving down & left 
+        int currentRow = row + 1;
+        int currentColumn = column - 1;
+        while (currentRow < 8 && currentColumn > -1 && board[currentRow][currentColumn] == null) { 
+            result.add({currentRow, currentColumn});
+            currentRow++;
+            currentColumn--;
+        } 
+
+        if (currentRow < 8 && currentColumn > -1) { 
+            result.add({currentRow, currentColumn}); 
+        }
+
+        // Moving left
+        int currentColumn = column - 1;
+        while (currentColumn > -1 && board[row][currentColumn] == null) {
+            result.add({row, currentColumn});
+            currentColumn--; 
+        }
+
+        if (currentColumn > -1) {
+            result.add({row, currentColumn});
+        }
+
+        // Moving up & left 
+        int currentRow = row - 1;
+        int currentColumn = column - 1;
+        while (currentRow > -1 && currentColumn > -1 && board[currentRow][currentColumn] == null) { 
+            result.add({currentRow, currentColumn});
+            currentRow--;
+            currentColumn--;
+        } 
+
+        if (currentRow > -1 && currentColumn > -1) { 
+            result.add({currentRow, currentColumn}); 
+        }
+
+        return result;
+    }
+
+    public boolean checkMove(int newRow, int newColumn, ChessPiece queen, GameState gameState) {
+        int[] desiredMove = {newRow, newColumn};
+        int[][] possibleMoves = validMoves((Queen) queen, gameState);
+        
+        for (int[] move : possibleMoves) {
+            if (Arrays.equals(desiredMove, move)) {
+                return true;
+            }
+        }
+
+        return false;
     }
   
 }
