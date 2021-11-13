8d16e5cc77b9684fd37deb2de2ca3fc6c8b9922a
akazee <akazee.135@gmail.com>
2021-11-12 20:59:25 -0500

    implemented toArrayMoves

--- src/UseCases/CheckPlayerMove.java
+++ src/UseCases/CheckPlayerMove.java
@@ -3,6 +3,9 @@ package UseCases;
 import Entities.ChessPiece;
 import Other.GameState;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public abstract class CheckPlayerMove {
     
     abstract int[][] validMoves(ChessPiece piece, GameState gameState); 
@@ -21,10 +24,15 @@ public abstract class CheckPlayerMove {
     }
 
     /**
-     *
-     * @return
+     * This is a helper method that takes in an array list if integer arrays and
+     * transforms its items into a 2d array of the same size.
      */
-    public int[][] toArrayMoves(){
-        // to be implemented
+    protected int[][] toArrayMoves(ArrayList<int[]> arr){
+        int[][] newArr = new int[arr.size()][];
+
+        for(int i = 0; i < arr.size(); i ++){
+            newArr[i] = arr.get(i);
+        }
+        return newArr;
     }
 }
