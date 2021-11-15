1f15c69996d0834a37a3d11dfc6de5eab568f8ee
meetp-03 <meetv.patel@mail.utoronto.ca>
2021-11-15 00:17:48 -0500

    Fixed Pawn unit tests caused by issues using "@Before" and incorrect parameters

--- test/PawnMoveTest.java
+++ test/PawnMoveTest.java
@@ -13,59 +13,43 @@ public class PawnMoveTest {
 
     GameState state;
 
-
     // Testing that the pawn can move two spaces up at the beginning of the game
-    @Before
-    public void setupMoveDouble() {
-        LoadGame init = new LoadGame();
-        GameState state = init.loadGame();
-    }
-
     @Test(timeout = 100)
     public void testMoveDouble() {
+        LoadGame init = new LoadGame();
+        state = init.loadGame();
         int[] arr = {6, 3, 4, 3};
         state.makeMove(arr);
-        assertNull(state.getBoard()[3][6]);
+        assertEquals('p', state.getChessPieceLetter(4, 3));
     }
-
 
 
     // Testing that the pawn can correctly attack another piece
-    @Before
-    public void setupAttack() {
+    @Test(timeout = 100)
+    public void testAttack() {
         LoadGame init = new LoadGame();
         state = init.loadGame();
         int[] arr = {6, 3, 4, 3};
         int[] arr2 = {1, 4, 3, 4};
+        int[] arr3 = {4, 3, 3, 4};
         state.makeMove(arr);
         state.makeMove(arr2);
-    }
-
-    @Test(timeout = 100)
-    public void testAttack() {
-        int[] arr = {4, 3, 3, 4};
-        state.makeMove(arr);
+        state.makeMove(arr3);
         assertEquals('p', state.getChessPieceLetter(3, 4));
     }
 
 
-
     //Testing that the pawn cannot make an invalid move - in this case moving 2 spaces up when it is not in its
     // initial position
-    @Before
-    public void setupInvalidMove() {
+    @Test(timeout = 100)
+    public void testInvalidMove() {
         LoadGame init = new LoadGame();
         state = init.loadGame();
         int[] arr = {6, 3, 4, 3};
         int[] arr2 = {1, 4, 3, 4};
+        int[] arr3 = {4, 3, 2, 3};
         state.makeMove(arr);
         state.makeMove(arr2);
-    }
-
-    @Test(timeout = 100)
-    public void testInvalidMove() {
-        int[] arr = {4, 3, 2, 3};
-        state.makeMove(arr);
-        assertFalse(state.makeMove(arr));
+        assertFalse(state.makeMove(arr3));
     }
 }
