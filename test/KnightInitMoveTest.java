cfd6e158df84105ee2592280878ea8077eae5f46
meetp-03 <meetv.patel@mail.utoronto.ca>
2021-11-14 23:34:20 -0500

    Fixed errors in Knight unit tests caused by issues using @Before

--- test/KnightInitMoveTest.java
+++ test/KnightInitMoveTest.java
@@ -31,46 +31,32 @@ public class KnightInitMoveTest {
     }
 
 
-
     // Testing that the Knight piece can move 2 units to the right and 1 unit up
-    @Before
-    public void setupMoveTwo() {
-        LoadGame init = new LoadGame();
-        state = init.loadGame();
-        int[] arr = {7, 1, 5, 2};
-        int[] arr2 = {1, 0, 3, 0};
-        state.makeMove(arr);
-        state.makeMove(arr2);
-    }
-
     @Test(timeout = 100)
     public void testMoveTwo() {
-        int[] arr = {5, 2, 4, 4};
+        int[] arr = {7, 1, 5, 2};
+        int[] arr2 = {1, 0, 3, 0};
+        int[] arr3 = {5, 2, 4, 4};
         state.makeMove(arr);
+        state.makeMove(arr2);
+        state.makeMove(arr3);
         assertEquals('k', state.getChessPieceLetter(4, 4));
     }
 
 
-
     // Testing that the Knight piece can move 2 units to the left and 1 unit down
-    @Before
-    public void setupMoveThree() {
-        LoadGame init = new LoadGame();
-        state = init.loadGame();
+    @Test(timeout = 100)
+    public void testMoveThree() {
         int[] arr = {7, 1, 5, 2};
         int[] arr2 = {1, 0, 3, 0};
         int[] arr3 = {5, 2, 4, 4};
         int[] arr4 = {1, 1, 3, 1};
+        int[] arr5 = {4, 4, 5, 2};
         state.makeMove(arr);
         state.makeMove(arr2);
         state.makeMove(arr3);
         state.makeMove(arr4);
-    }
-
-    @Test(timeout = 100)
-    public void testMoveThree() {
-        int[] arr = {4, 4, 5, 2};
-        state.makeMove(arr);
+        state.makeMove(arr5);
         assertEquals('k', state.getChessPieceLetter(5, 2));
     }
 }
