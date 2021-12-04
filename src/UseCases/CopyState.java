2411efbfc5f3a90ee4e182b4739ae6e2c2ce6afb
akazee <akazee.135@gmail.com>
2021-12-04 01:11:35 -0500

    added comments

--- src/UseCases/CopyState.java
+++ src/UseCases/CopyState.java
@@ -10,14 +10,23 @@ import UseCases.ChessPieceFactory;
  */
 public class CopyState {
 
+    /**
+     * This method takes in a GameState and iterates through every position in its chessboard
+     * and adds a duplicate of each chess piece within the same position in a new GameState object.
+     *
+     * Returns a duplicate of the GameState object given as a parameter.
+     */
     public GameState copyState(GameState state){
         ChessPiece[][] currBoard = state.getBoard();
         GameState newState = new GameState();
         ChessPieceFactory factory = new ChessPieceFactory();
 
+        // Iterating through each position
         for(int i = 0; i < 8; i++){
              for(int y = 0; y < 8; y++){
 
+                 // Uses the ChessPieceFactory's .getCopy() method to create and
+                 // store duplicate chess pieces
                  if(currBoard[i][y] != null){
                      ChessPiece piece = factory.getCopy(currBoard[i][y]);
                      newState.addChessPiece(piece);
